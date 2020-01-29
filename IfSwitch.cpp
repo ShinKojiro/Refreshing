4669a76e9ae78bd106d2226f3448114e9de9b6fd
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-01-28 23:52:25 -0600

    Added a Rock Paper Scissors game.

--- IfSwitch.cpp
+++ IfSwitch.cpp
@@ -3,81 +3,81 @@
 
 using namespace std;
 
-int main()
-{
-	/*int x = 2;
-
-	cout << "start\n";
-
-	if (x <= 3)
-	{
-		if (x != 0)
-		{
-			cout << "Hellow from the second if.\n";
-		}
-		else
-		{
-			cout << "Hello from the else\n";
-		}
-
-	}
-	
-	cout << "End\n";
-	cout << "Start again\n";*/
-
-
-	//skips the 2nd if because the 1st if was not satisfied
-	/*if (x > 3)
-	{
-		if (x != 0)
-		{
-			cout << "Hello from the second if.\n";
-		}
-		else
-		{
-			cout << "Hello from the else.\n";
-		}
-	}
-	cout << "End Again\n";*/
-
-	char grade;
-	char enterAgain = 'y';
-
-	cout << "Enter the students letter grade: ";
-
-	while (enterAgain == 'y' || enterAgain == 'Y')
-	{
-		cin >> grade;
-		switch (grade)
-		{
-		case 'A':
-		case'a':
-			cout << "Student made an A, thats great\n";
-			break;
-		case 'B':
-		case 'b':
-			cout << "Student made a B, its good\n";
-			break;
-		case 'C':
-		case 'c':
-			cout << "Student made a C. They should try harder\n";
-			break;
-		case 'D':
-		case 'd':
-			cout << "Student made a D. They have work to do\n";
-			break;
-		case 'E':
-		case 'e':
-			cout << "Student made an E, for effort. Not enough effort, however\n";
-			break;
-		case 'F':
-		case 'f':
-			cout << "Student made an F. Must repeeat the class\n";
-			break;
-		default:
-			cout << "That is not an appropriate entry\n";
-		}
-		cout << "Enter another grade? ";
-		cin >> enterAgain;
-	}
-}
+//int main()
+//{
+//	/*int x = 2;
+//
+//	cout << "start\n";
+//
+//	if (x <= 3)
+//	{
+//		if (x != 0)
+//		{
+//			cout << "Hellow from the second if.\n";
+//		}
+//		else
+//		{
+//			cout << "Hello from the else\n";
+//		}
+//
+//	}
+//	
+//	cout << "End\n";
+//	cout << "Start again\n";*/
+//
+//
+//	//skips the 2nd if because the 1st if was not satisfied
+//	/*if (x > 3)
+//	{
+//		if (x != 0)
+//		{
+//			cout << "Hello from the second if.\n";
+//		}
+//		else
+//		{
+//			cout << "Hello from the else.\n";
+//		}
+//	}
+//	cout << "End Again\n";*/
+//
+//	/*char grade;
+//	char enterAgain = 'y';
+//
+//	cout << "Enter the students letter grade: ";
+//
+//	while (enterAgain == 'y' || enterAgain == 'Y')
+//	{
+//		cin >> grade;
+//		switch (grade)
+//		{
+//		case 'A':
+//		case'a':
+//			cout << "Student made an A, thats great\n";
+//			break;
+//		case 'B':
+//		case 'b':
+//			cout << "Student made a B, its good\n";
+//			break;
+//		case 'C':
+//		case 'c':
+//			cout << "Student made a C. They should try harder\n";
+//			break;
+//		case 'D':
+//		case 'd':
+//			cout << "Student made a D. They have work to do\n";
+//			break;
+//		case 'E':
+//		case 'e':
+//			cout << "Student made an E, for effort. Not enough effort, however\n";
+//			break;
+//		case 'F':
+//		case 'f':
+//			cout << "Student made an F. Must repeeat the class\n";
+//			break;
+//		default:
+//			cout << "That is not an appropriate entry\n";
+//		}
+//		cout << "Enter another grade? ";
+//		cin >> enterAgain;
+//	}*/
+//}
---
+++ RockPaperScissors.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	char player1Choice;
+	char player2Choice;
+	char playAgain = 'y';
+
+	cout << "Simple Rock paper scissors game.\n";
+	cout << "R for rock\nP for paper\nS for scissors";
+
+	while (playAgain == 'y' || playAgain == 'Y')
+	{
+		cout << "\nPlayer 1 choice: ";
+	    err1:cin >> player1Choice;
+		//Though goto is not reccomended, it works well here. I think as long as they are labled them properly and dont go too far out of the way it can work right.
+
+		switch (player1Choice)
+		{
+		case 'r':
+		case 'R':
+			//redundant player 2 choices to make goto errors work cleaner
+			cout << "\nPlayer 2 choice: ";
+		    err2:cin >> player2Choice;
+			switch (player2Choice)
+			{
+			case 'r':
+			case 'R':
+				cout << "Both of you chose rock ! Tie.\n";
+				break;
+			case 'p':
+			case 'P':
+				cout << "Player 2's Paper wrapped up Player 1's Rock !\n";
+				break;
+			case 's':
+			case 'S':
+				cout << "Player 1's Rock broke Player 2's Scissors !\n";
+				break;
+			default:
+				cout << "That was not a correct choice, Player 2. Enter again: ";
+				goto err2;
+			}
+			break;
+		case 'p':
+		case 'P':
+			cout << "\nPlayer 2 choice: ";
+		    err3:cin >> player2Choice;
+			switch (player2Choice)
+			{
+			case 'r':
+			case 'R':
+				cout << "Player 1's paper wrapped up Player 2's Rock !\n";
+				break;
+			case 'p':
+			case 'P':
+				cout << "Both of you chose paper ! Tie.\n";
+				break;
+			case 's':
+			case 'S':
+				cout << "Player 2's Scissors cut up Player 1's Paper !\n";
+				break;
+			default:
+				cout << "That was not a correct choice, Player 2. Enter again: ";
+				goto err3;
+			}
+			break;
+		case 's':
+		case 'S':
+			cout << "\nPlayer 2 choice: ";
+		    err4:cin >> player2Choice;
+			switch (player2Choice)
+			{
+			case 'r':
+			case 'R':
+				cout << "Player 2's Rock beat up player 1's Scissors !\n";
+				break;
+			case 'p':
+			case 'P':
+				cout << "Player 1's Scissors shredded player 2's Paper !\n";
+				break;
+			case 's':
+			case 'S':
+				cout << "Both of you chose Scissors ! Tie.\n";
+				break;
+			default:
+				cout << "That was not a correct choice, player 2. Enter again: ";
+				goto err4;
+			}
+			break;
+		default:
+			cout << "That was not a correct choice, player 1. Enter again: ";
+			goto err1;
+		}
+		cout << "Do you want to play again? ";
+		cin >> playAgain;
+	}
+}
