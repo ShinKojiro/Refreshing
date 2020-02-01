855a6a9b9c6946d62fa581bffdeccce0d729377a
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2020-02-01 15:16:14 -0600

    Celcius to Farenheit Coverter

---
+++ FahrenheitCelsius.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	int fahrenheit = 0;
+	int celsius = 100;
+	char reEnter = 'y';
+
+	//static portion
+	//while (celsius > -100)
+	//{
+	//	//cin >> celsius;
+	//	fahrenheit = ((9 * celsius) / 5) + 32;
+	//	cout << endl << fahrenheit << " " << celsius;
+	//	celsius = celsius - 1;
+	//}
+
+	//User Input portion
+	while (reEnter == 'y' || reEnter == 'Y')
+	{
+		cin >> celsius;
+		fahrenheit = ((9 * celsius) / 5) + 32;
+		cout << endl << fahrenheit << " " << celsius;
+	}
+}
